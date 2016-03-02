b0a909e756abb896db0208b38ed04c3c494bd3b2
Dan Wald <pirate.coffee@gmail.com>
2016-03-02 13:36:09 -0600

    Add Git pull

--- boot.sh
+++ boot.sh
@@ -5,9 +5,9 @@ sudo rdate time.nist.gov
 cd /home/pi/FLACS
 
 
-#These lines pull updates from the git repo - uncomment to pull newest code on boot.
-#sudo git fetch --all
+#These lines pull updates from the git repo - comment to stop pull of newest code on boot.
+sudo git fetch --all
 
-#sudo git reset --hard origin/master
+sudo git reset --hard origin/master
 
 sudo python fablab.py
